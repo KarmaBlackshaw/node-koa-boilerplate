513b07700ff17f303f9aeb812a12fae336816398
Karma Blackshaw <irenesejah29@gmail.com>
2021-09-09 16:21:33 +0800

    update knexhelper

--- utilities/knexHelper.js
+++ utilities/knexHelper.js
@@ -1,119 +1,162 @@
-const _flatten = require('lodash/flatten')
+const store = require('@store')
+
+// utilities
+const { getKey, isPOJO } = require('@utilities/helpers')
+
+// libraries
 const _isEmpty = require('lodash/isEmpty')
-const isObject = obj => Object.prototype.toString.call(obj) === '[object Object]'
+const _isNil = require('lodash/isNil')
 
-module.exports = knex => {
-  const knexHelper = {
-    raw: qb => knex.raw(qb),
+// helpers
+const raw = qb => store.knex.raw(qb)
+const isArray = item => item && Array.isArray(item)
+const isDate = date => !isNaN(new Date(date).getTime())
 
-    aliasify (obj) {
-      const fields = ['id', ..._flatten(Object.values(obj))]
-      const commons = fields.reduce((acc, curr) => fields.filter(x => x === curr).length > 1 ? [...acc, curr] : acc, [])
+const knexHelper = {
+  raw,
 
-      const appendCommon = (alias, common) => {
-        const newCommon = (common.length === 2 ? common.toUpperCase() : `_${common}`)
-        return alias.toLowerCase() + newCommon
-      }
+  jsonObject (data, isRaw = 1) {
+    const cols = Object
+      .keys(data)
+      .reduce((acc, curr) => [...acc, `"${curr}", ${data[curr]}`], [])
+      .join(', ')
+      .trim()
 
-      const alias = (array, alias) => array.map(x => {
-        return commons.includes(x.toLowerCase()) && x !== '*'
-          ? `${alias}.${x} as ${appendCommon(alias, x)}`
-          : `${alias}.${x}`
-      })
+    return isRaw
+      ? raw(`JSON_OBJECT(${cols})`)
+      : `JSON_OBJECT(${cols})`
+  },
 
-      return _flatten(Object.keys(obj).map(key => alias(obj[key], key)))
-    },
+  jsonExtract (column, key, raw) {
+    return raw
+      ? knexHelper.raw(`JSON_EXTRACT(${column}, '$.${key}')`)
+      : `JSON_EXTRACT(${column}, '$.${key}')`
+  },
 
-    jsonObject ({ alias, data }) {
-      const cols = Object
-        .keys(data)
-        .reduce((acc, curr) => [...acc, `"${curr}", ${data[curr]}`], [])
-        .join(', ')
-        .trim()
-
-      return alias
-        ? `JSON_OBJECT(${cols}) as ${alias}`
-        : `JSON_OBJECT(${cols})`
-    },
-
-    _jsonObject (data, raw) {
-      return raw
-        ? knexHelper.raw(knexHelper.jsonObject({ data }))
-        : knexHelper.jsonObject({ data })
-    },
-
-    jsonExtract (column, key, alias) {
-      return alias
-        ? `JSON_EXTRACT(${column}, '$.${key}') as ${alias}`
-        : `JSON_EXTRACT(${column}, '$.${key}')`
-    },
-
-    rowNumber (column, { alias = 'rowNumber', order = 'asc' } = {}) {
-      if (!column) {
-        throw new Error(`rowNumber expected a value. Received ${column}`)
-      }
-
-      return alias
-        ? `row_number() over (order by ${column} ${order}) as ${alias}`
-        : `row_number() over (order by ${column} ${order})`
-    },
-
-    makeQuery ({ knex, filterBy, q, page, rows, sortBy, sort, dateBy, dateFrom, dateTo, isCount }) {
-      if (Number(page) && Number(rows) && !isCount) {
-        knex.limit(rows).offset(rows * (page - 1))
-      }
-
-      if (sortBy && sort) {
-        if (Array.isArray(sortBy)) {
-          for (let i = 0; i < sortBy.length; i++) {
-            const curr = sortBy[i]
-            knex.orderBy(curr[0], curr[1])
-          }
-        } else {
-          knex.orderBy(sortBy, sort)
-        }
-      }
-
-      if (dateBy && dateFrom && dateTo) {
-        knex.whereBetween(dateBy, [dateFrom, dateTo])
-      }
-
-      if (filterBy && q) {
-        if (Array.isArray(filterBy)) {
-          knex.where(function () {
-            for (let i = 0; i < filterBy.length; i++) {
-              const curr = filterBy[i]
-              isObject(curr)
-                ? this.orWhere(curr.value, curr.operator, q)
-                : this.orWhere(curr, 'LIKE', `%${q}%`)
-            }
-          })
-        } else {
-          const isFilterByObject = isObject(filterBy)
-
-          if (isFilterByObject && filterBy.method === 'having') {
-            if (filterBy.operator) {
-              knex.having(filterBy.value, filterBy.operator, q)
-            } else {
-              knex.having(filterBy.value, 'LIKE', `%${q}%`)
-            }
-          } else if (isFilterByObject && filterBy.operator) {
-            knex.where(filterBy.value, filterBy.operator, q)
-          } else {
-            knex.where(filterBy, 'LIKE', `%${q}%`)
-          }
-        }
-      }
-    },
-
-    wrapCase (caseStatements) {
-      if (_isEmpty(caseStatements)) {
-        throw new Error(`Case statements expected a value. Received ${caseStatements}`)
-      }
-
-      return `(CASE ${caseStatements} END)`
+  /**
+    Usage:
+    const dictionary = {
+      id: 'res.id',
+      fname: 'res.fname',
+      mname: 'res.mname',
+      lname: 'res.lname'
     }
-  }
 
-  return knexHelper
+    const filterColumns = {
+      ...dictionary,
+      name: ['res.fname', 'res.mname', 'res.lname']
+    }
+
+    const sortColumns = {
+      ...dictionary,
+      alias: 'alias'
+    }
+
+    makeQuery({
+      ...{ filterBy, q, filterColumns },
+      ...{ sortBy, sort, sortColumns },
+      ...{ page, rows },
+      knex,
+      isCount
+    })
+   */
+
+  makeQuery ({ knex, filterBy, q, filterColumns, page, rows, sortBy, sort, sortColumns, dateBy, dateFrom, dateTo, dateColumns, isCount }) {
+    if (Number(page) && Number(rows) && !isCount) {
+      knex.limit(rows).offset(rows * (page - 1))
+    }
+
+    if (isArray(sortBy) && isArray(sort) && sortColumns && !isCount) {
+      for (let i = 0; i < sortBy.length; i++) {
+        const currSortBy = sortColumns[sortBy[i]]
+        const currSort = sort[i]
+
+        if (!currSortBy || !currSort) {
+          continue
+        }
+
+        knex.orderBy(currSortBy, currSort)
+      }
+    }
+
+    if (isArray(dateBy) && isArray(dateFrom) && isArray(dateTo) && dateColumns) {
+      knex.where(function () {
+        for (let i = 0; i < dateBy.length; i++) {
+          const currDateBy = dateColumns[dateBy[i]]
+          const currDateFrom = dateFrom[i]
+          const currDateTo = dateTo[i]
+
+          if (!isDate(currDateFrom) || !isDate(currDateTo) || !currDateBy) {
+            continue
+          }
+
+          this.orWhereBetween(currDateBy, [currDateFrom, currDateTo])
+        }
+      })
+    }
+
+    if (isArray(filterBy) && isArray(q) && filterColumns) {
+      knex.where(function () {
+        for (let i = 0; i < q.length; i++) {
+          const currQ = q[i]
+          const currFilter = filterColumns[filterBy[i]]
+
+          if (!currQ || !currFilter) {
+            continue
+          }
+
+          if (Array.isArray(currFilter)) {
+            this.where(function () {
+              for (let j = 0; j < currFilter.length; j++) {
+                this.orWhere(currFilter[j], 'like', `%${currQ}%`)
+              }
+            })
+
+            continue
+          }
+
+          this.where(currFilter, 'like', `%${currQ}%`)
+        }
+      })
+    }
+  },
+
+  findBy ({ knex, filterBy, q, dictionary }) {
+    try {
+      const filterByColumn = getKey(filterBy, dictionary)
+      let counter = 0
+
+      if (filterByColumn && !_isNil(q)) {
+        counter++
+        knex.where(filterByColumn, q)
+      }
+
+      if (isPOJO(filterBy)) {
+        for (const key in filterBy) {
+          if (dictionary[key] && !_isNil(filterBy[key])) {
+            counter++
+            knex.where(dictionary[key], filterBy[key])
+          }
+        }
+      }
+
+      if (!counter) {
+        knex.whereRaw('0 = 1')
+      }
+    } catch (error) {
+      throw error
+    }
+  },
+
+  wrapCase (caseStatements, raw) {
+    if (_isEmpty(caseStatements)) {
+      throw new Error(`Case statements expected a value. Received ${caseStatements}`)
+    }
+
+    return raw
+      ? raw(`(CASE ${caseStatements} END)`)
+      : `(CASE ${caseStatements} END)`
+  }
 }
+
+module.exports = knexHelper
