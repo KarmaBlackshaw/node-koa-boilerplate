31123acdc177652f8065a311609594b08165841a
Karma Blackshaw <irenesejah29@gmail.com>
2021-12-22 00:49:30 +0800

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,34 +5,39 @@ module.exports = {
     es6: true,
     node: true
   },
-
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
-
   parserOptions: {
     ecmaVersion: 2018
   },
-
   extends: ['standard'],
-
   rules: {
-    indent: ['error', 2],
-    'comma-dangle': ['error', 'never'],
-    'comma-spacing': ['error', { before: false, after: true }],
-    'prefer-const': 'error',
-    'arrow-parens': [2, 'as-needed'],
-    'no-await-in-loop': 'error',
+    'no-await-in-loop': 'off',
     'no-console': 'off',
     'no-useless-catch': 'off',
+    'prefer-regex-literals': 'off',
+    'default-case': 'off',
+    'comma-dangle': ['error', 'never'],
+    'prefer-const': 'error',
+    'arrow-parens': [2, 'as-needed'],
     'object-curly-spacing': ['error', 'always'],
+    curly: [2, 'all'],
+    indent: ['error', 2, {
+      SwitchCase: 1
+    }],
+    'comma-spacing': ['error', {
+      before: false, after: true
+    }],
+    'brace-style': ['error', '1tbs', {
+      allowSingleLine: false
+    }],
     'no-unused-vars': ['error',
       {
         argsIgnorePattern: 'next|_.+',
         varsIgnorePattern: '_'
       }
-    ],
-    'default-case': 'error'
+    ]
   }
 }
