0670e8d325a90d494b979ded8188ac8f920276a1
Karma Blackshaw <irenesejah29@gmail.com>
2021-12-27 14:29:12 +0800

    update jobs and listeners structure

--- listeners/index.js
+++ listeners/index.js
@@ -1,16 +1,29 @@
+// libs
 const Promise = require('bluebird')
+const minimatch = require('minimatch')
 
 const fs = Promise.promisifyAll(require('fs'))
 const path = require('path')
+
+const exclude = [
+  '_*',
+  '.*'
+]
 
 module.exports = async () => {
   const files = await fs.readdirAsync(path.join(__dirname, 'items'))
 
   files.forEach(file => {
     try {
+      const isExcluded = exclude.some(glob => !!minimatch(file, glob))
+
+      if (isExcluded) {
+        return
+      }
+
       require(path.join(__dirname, 'items', file))()
     } catch (error) {
-      throw new Error(`Error on file listeners/items/${file}`)
+      throw new Error(`Error on file jobs/items/${file}`)
     }
   })
 }
