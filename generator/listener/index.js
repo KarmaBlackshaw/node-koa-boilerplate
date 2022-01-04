4b797681b7fce95f94f61ecb9c014f9341f3e009
Karma Blackshaw <irenesejah29@gmail.com>
2022-01-04 13:37:15 +0800

    add listener generator

---
+++ generator/listener/index.js
@@ -0,0 +1,46 @@
+const chalk = require('chalk')
+
+module.exports = plop => {
+  plop.load('../utility', {}, {
+    helpers: true
+  })
+
+  plop.setGenerator('module', {
+    description: 'Generate listener file',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Generate listener file:',
+        validate: function (input) {
+          if (isNaN(input)) {
+            return true
+          }
+
+          const error = [
+            '-------------------------------------------------------',
+            ':::::::::: :::::::::  :::::::::   ::::::::  :::::::::',
+            ':+:        :+:    :+: :+:    :+: :+:    :+: :+:    :+:',
+            '+:+        +:+    +:+ +:+    +:+ +:+    +:+ +:+    +:+ ',
+            '+#++:++#   +#++:++#:  +#++:++#:  +#+    +:+ +#++:++#:  ',
+            '+#+        +#+    +#+ +#+    +#+ +#+    +#+ +#+    +#+ ',
+            '#+#        #+#    #+# #+#    #+# #+#    #+# #+#    #+# ',
+            '########## ###    ### ###    ###  ########  ###    ### ',
+            '-------------------------------------------------------'
+          ].join('\n')
+
+          console.log(chalk.bold.red(error))
+          console.log(chalk.bold.red('Number is not a valid file name!\n'))
+        }
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../../listeners/items/{{kebabCase name}}.js',
+        templateFile: './templates/listener.hbs',
+        skipIfExists: false
+      }
+    ]
+  })
+}
