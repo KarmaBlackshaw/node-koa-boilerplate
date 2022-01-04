a60adcb76fe847b2f2bf1728b0e10d8485e7ab73
Karma Blackshaw <irenesejah29@gmail.com>
2022-01-04 14:48:43 +0800

    update koa parser

--- services/http/middleware/koa-parser.js
+++ services/http/middleware/koa-parser.js
@@ -1,28 +1,29 @@
 
 const bodyParser = require('koa-bodyparser')
-const formidable = require('koa2-formidable')
 
 module.exports = () => {
   return async (ctx, next) => {
-    const cType = ctx.request.header['content-type']
-    let origin = ctx.request.header.origin ? ctx.request.header.origin : ''
-    if (origin) {
-      origin = origin.replace('http://', '').split(':')[0]
-    }
+    const contentType = ctx.request.header['content-type']
+    const originHeader = ctx.request.header.origin
+    const origin = String(originHeader).replace('http://', '').split(':')[0]
 
-    if (!cType && (!origin || process.env.HOST_ORIGIN.indexOf(origin) === -1)) {
+    if (!contentType && !origin) {
       ctx.request.header['content-type'] = 'application/json; charset=utf-8'
       ctx.request.header['content-encoding'] = 'gzip'
     }
 
-    const req = ctx.is('application/json')
-      ? bodyParser({ enableTypes: ['json', 'text'], jsonLimit: '200mb', formLimit: '200mmb', textLimit: '20mb' })
-      : formidable({})
+    const req = bodyParser({
+      enableTypes: ['json', 'text'],
+      jsonLimit: '200mb',
+      formLimit: '200mmb',
+      textLimit: '20mb'
+    })
 
     try {
       await req(ctx, next)
     } catch (err) {
-      console.log(err)
+      console.log(ctx.request)
+      throw err
     }
   }
 }
