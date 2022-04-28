1876f605c7c24415b48f39a2a1890fd5dff543ac
Karma Blackshaw <irenesejah29@gmail.com>
2022-04-28 21:37:38 +0800

    update module alias and utilities

--- utilities/module-alias.js
+++ utilities/module-alias.js
@@ -8,7 +8,6 @@ module.exports = dir => {
   moduleAlias.addAliases({
     '@': path.join(dir),
     '@utilities': path.join(dir, 'utilities'),
-    '@helpers': path.join(dir, 'utilities/helpers'),
     '@listeners': path.join(dir, 'listeners'),
     '@jobs': path.join(dir, 'jobs'),
     '@services': path.join(dir, 'services'),
