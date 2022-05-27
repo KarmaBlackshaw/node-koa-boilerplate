f9cf7da66788cbe681c47398bcd6a3cc715f3d6a
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-27 15:02:36 +0800

    refactor: add env check

--- bootstrap/env-check.js
+++ bootstrap/env-check.js
@@ -1,55 +1,56 @@
 const Joi = require('joi')
 
 module.exports = async () => {
-  const schema = Joi.object({
+  const schema = Joi
+    .object({
     /**
      * Database schema
      */
-    DB_HOST: Joi.string()
-      .required(),
-    DB_USER: Joi.string()
-      .required(),
-    DB_PASS: Joi.string()
-      .required(),
-    DB_NAME: Joi.string()
-      .required(),
+      DB_HOST: Joi.string()
+        .required(),
+      DB_USER: Joi.string()
+        .required(),
+      DB_PASS: Joi.string()
+        .required(),
+      DB_NAME: Joi.string()
+        .required(),
 
-    /**
+      /**
      * Node environment
      */
-    NODE_ENV: Joi.string()
-      .optional(),
+      NODE_ENV: Joi.string()
+        .optional(),
 
-    APP_SECRET_KEY: Joi.string()
-      .guid()
-      .required(),
-    APP_PORT: Joi.number()
-      .required(),
+      APP_SECRET_KEY: Joi.string()
+        .guid()
+        .required(),
+      APP_PORT: Joi.number()
+        .required(),
 
-    SOCKET_PORT: Joi.number()
-      .required(),
+      SOCKET_PORT: Joi.number()
+        .required(),
 
-    STATIC_ASSET_PATH: Joi.string()
-      .required(),
+      STATIC_ASSET_PATH: Joi.string()
+        .required(),
 
-    REDIS_HOST: Joi.string()
-      .required(),
-    REDIS_PORT: Joi.number()
-      .required(),
+      REDIS_HOST: Joi.string()
+        .required(),
+      REDIS_PORT: Joi.number()
+        .required(),
 
-    AWS_ACCESS_KEY_ID: Joi.string()
-      .optional()
-      .allow(''),
-    AWS_SECRET_KEY_ACCESS: Joi.string()
-      .optional()
-      .allow(''),
-    AWS_REGION: Joi.string()
-      .optional()
-      .allow(''),
-    AWS_BUCKET_NAME: Joi.string()
-      .optional()
-      .allow('')
-  })
+      AWS_ACCESS_KEY_ID: Joi.string()
+        .optional()
+        .allow(''),
+      AWS_SECRET_KEY_ACCESS: Joi.string()
+        .optional()
+        .allow(''),
+      AWS_REGION: Joi.string()
+        .optional()
+        .allow(''),
+      AWS_BUCKET_NAME: Joi.string()
+        .optional()
+        .allow('')
+    })
     .options({ allowUnknown: true })
 
   try {
