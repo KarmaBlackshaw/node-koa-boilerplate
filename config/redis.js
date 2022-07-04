3ad996b52313b2e2042a72399cdaf446fb3bf2d1
Karma Blackshaw <irenesejah29@gmail.com>
2022-07-04 17:18:38 +0800

    ref: restructure config redis

--- config/redis.js
+++ config/redis.js
@@ -1,53 +1,63 @@
 const redis = require('redis')
 const env = require('@config/env')
 
-module.exports = {
+const state = {
   client: null,
   publisher: null,
-  subscriber: null,
+  subscriber: null
+}
 
-  async start () {
-    this.client = await this.connect()
-    this.publisher = await this.duplicate()
-    this.subscriber = await this.duplicate()
-  },
-
-  async duplicate () {
-    const client = this.client.duplicate()
-
-    await client.connect()
-
-    return client
-  },
-
-  async connect () {
-    if (this.client) {
-      console.log('A client is already registered. Use `duplicate` instead')
-      return this.client
-    }
-
-    const client = redis.createClient({
-      url: `redis://@${env.REDIS_HOST}:${env.REDIS_PORT}`
-    })
-
-    await client.connect()
-
-    client.on('error', e => console.log('Redis instance: ', e.stack))
-
-    return client
-  },
-
-  emitSocketAdmin (namespace, event, data) {
-    return this.publisher.publish(
-      `socket:admin_events:${namespace}:${event}`,
-      JSON.stringify(data)
-    )
-  },
-
-  emitSocketUser (namespace, event, data) {
-    return this.publisher.publish(
-      `socket:user_events:${namespace}:${event}`,
-      JSON.stringify(data)
-    )
+async function connect () {
+  if (state.client) {
+    console.log('A client is already registered. Use `duplicate` instead')
+    return state.client
   }
+
+  const isntance = redis.createClient({
+    url: `redis://@${env.REDIS_HOST}:${env.REDIS_PORT}`
+  })
+
+  await isntance.connect()
+
+  isntance.on('error', e => console.log('Redis instance: ', e.stack))
+
+  return isntance
+}
+
+async function start () {
+  state.client = await connect()
+  state.publisher = await duplicate()
+  state.subscriber = await duplicate()
+}
+
+async function duplicate () {
+  const instance = state.client.duplicate()
+
+  await instance.connect()
+
+  return instance
+}
+
+function emitSocketAdmin (namespace, event, data) {
+  return state.publisher.publish(
+    `socket:admin_events:${namespace}:${event}`,
+    JSON.stringify(data)
+  )
+}
+
+function emitSocketUser (namespace, event, data) {
+  return state.publisher.publish(
+    `socket:user_events:${namespace}:${event}`,
+    JSON.stringify(data)
+  )
+}
+
+module.exports = {
+  client: state.client,
+  publisher: state.publisher,
+  subscriber: state.subscriber,
+  start,
+  duplicate,
+  emitSocketAdmin,
+  emitSocketUser
 }
