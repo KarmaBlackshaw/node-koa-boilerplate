72e0df842d8e172c3d265ca2ca1c8e4ebad93923
Karma Blackshaw <irenesejah29@gmail.com>
2022-07-28 18:26:50 +0800

    add prefix for console properties

--- config/console.js
+++ config/console.js
@@ -1,22 +1,50 @@
+// libs
 const path = require('path')
 const chalk = require('chalk')
 
-const methods = ['debug', 'log', 'warn', 'error']
-methods.forEach(methodName => {
-  const originalLoggingMethod = console[methodName]
+// constants
+const CWD = process.cwd()
 
-  console[methodName] = (firstArgument, ...otherArguments) => {
+// vars
+const methods = [
+  {
+    name: 'debug',
+    prefix: chalk.blue('[DEBUG]')
+  },
+  {
+    name: 'warn',
+    prefix: chalk.yellow('[WARN]')
+  },
+  {
+    name: 'error',
+    prefix: chalk.red('[ERROR]')
+  },
+  {
+    name: 'deprecate',
+    prefix: chalk.red('[DEPRECATED]')
+  },
+  {
+    name: 'log'
+  }
+]
+
+methods.forEach(method => {
+  const originalLoggingMethod = console[method.name] || console.log
+
+  console[method.name] = (firstArgument, ...otherArguments) => {
     const originalPrepareStackTrace = Error.prepareStackTrace
     Error.prepareStackTrace = (_, stack) => stack
     const callee = new Error().stack[1]
     Error.prepareStackTrace = originalPrepareStackTrace
-    const relativeFileName = path.relative(process.cwd(), callee.getFileName())
-    const prefix = chalk.cyan.underline(`${relativeFileName}:${callee.getLineNumber()}:`)
+
+    const filename = path.relative(CWD, callee.getFileName())
+    const source = chalk.cyan.underline(`${filename}:${callee.getLineNumber()}:`)
+    const sourceAndPrefix = [source, method.prefix].join(' ').trim()
 
     if (typeof firstArgument === 'string') {
-      originalLoggingMethod(prefix + ' ' + firstArgument, ...otherArguments)
+      originalLoggingMethod(sourceAndPrefix + ' ' + firstArgument, ...otherArguments)
     } else {
-      originalLoggingMethod(prefix, firstArgument, ...otherArguments)
+      originalLoggingMethod(sourceAndPrefix, firstArgument, ...otherArguments)
     }
   }
 })
