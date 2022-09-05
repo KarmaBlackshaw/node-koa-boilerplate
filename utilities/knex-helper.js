2b6bf0ab8c2d4d2a19eda0d1d6120f2f75f4839c
Ernie Jeash Villahermosa <40014179+KarmaBlackshaw@users.noreply.github.com>
2022-09-05 21:58:52 +0800

    chore: remove unused knex helpers

--- utilities/knex-helper.js
+++ utilities/knex-helper.js
@@ -1,69 +1,7 @@
 const knex = require('@config/knex')
 
-// utilities
-const { getKey } = require('@utilities/object')
-
-// libraries
-const _isNil = require('lodash/isNil')
-const _isPlainObject = require('lodash/isPlainObject')
-
 function raw (qb) {
   return knex.raw(qb)
-}
-
-/**
-  const dictionary = {
-    id: 'res.id',
-    fname: 'res.fname',
-    mname: 'res.mname',
-    lname: 'res.lname',
-  }
-
-  findBy({
-    ...{ filterBy, q, dictionary },
-    knex
-  })
-
-  API
-  @param {array|string} filterBy    ex: ['fname', 'mname'] or 'fname'
-  @param {array|string} q           ex: ['foo', 'bar'] or 'foo'
-
-  */
-function findBy ({ knex, filterBy, q, dictionary }) {
-  try {
-    const filterByColumn = getKey(filterBy, dictionary)
-    let hasFilter = false
-
-    if (filterByColumn && !_isNil(q)) {
-      hasFilter = true
-      knex.where(filterByColumn, q)
-    }
-
-    if (_isPlainObject(filterBy)) {
-      for (const key in filterBy) {
-        if (dictionary[key] && !_isNil(filterBy[key])) {
-          hasFilter = true
-          knex.where(dictionary[key], filterBy[key])
-        }
-      }
-    }
-
-    if (!hasFilter) {
-      knex.whereRaw('0 = 1')
-    }
-
-    knex.first()
-  } catch (error) {
-    throw error
-  }
-}
-
-function jsonObject (data) {
-  const cols = Object.entries(data)
-    .map(([key, value]) => `"${key}", ${value}`)
-    .join(',  ')
-
-  return raw(`JSON_OBJECT(${cols})`)
 }
 
 function jsonExtract (column, key) {
@@ -74,10 +12,26 @@ function wrapCase (caseStatements) {
   return raw(`(CASE ${caseStatements} END)`)
 }
 
+function createFindArguments (conditions) {
+  const filterBy = []
+  const q = []
+
+  for (const key in conditions) {
+    const value = conditions[key]
+
+    filterBy.push(key)
+    q.push(`"${value}"`)
+  }
+
+  return {
+    filter_by: filterBy,
+    q
+  }
+}
+
 module.exports = {
+  createFindArguments,
   raw,
-  findBy,
-  jsonObject,
   jsonExtract,
   wrapCase
 }
