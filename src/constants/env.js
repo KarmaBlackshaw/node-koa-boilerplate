fa969ea1315152e7898957b13bf2438ba0a4ad47
Ernie Jeash Villahermosa <ernie.villahermosa@outliant.com>
2024-10-28 23:42:32 +0800

    Refactor environment variables and remove AWS related configurations

--- src/constants/env.js
+++ src/constants/env.js
@@ -23,14 +23,6 @@ module.exports = {
     HOST: env.REDIS_HOST,
     PORT: env.REDIS_PORT
   },
-  AWS: {
-    ACCESS_KEY_ID: env.AWS_ACCESS_KEY_ID,
-    SECRET_KEY_ACCESS: env.AWS_SECRET_KEY_ACCESS,
-    REGION: env.AWS_REGION,
-    BUCKET: {
-      NAME: env.AWS_BUCKET_NAME
-    }
-  },
   JWT: {
     EXPIRES_IN: env.JWT_EXPIRES_IN,
     SECRET_KEY: env.JWT_SECRET_KEY
